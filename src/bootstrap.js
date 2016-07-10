d7ae8102ab312dba32f1c5b5036abfdf2db759c2
dmi3y <lapshukov@gmail.com>
2016-07-09 22:42:59 -0700

    cleanear import

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
-import appReducer from './reducers/index'
+import appReducer from './reducers'
 import App from './components/app'
 
 export default function bootstrap () {
