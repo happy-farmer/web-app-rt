007fd9a36eee87f0215d787ed290375b25a8d360
dmi3y <lapshukov@gmail.com>
2016-09-10 19:29:21 -0700

    editable description component

---
+++ src/components/editableDescription.js
@@ -0,0 +1,28 @@
+/**
+ * @module components/editableDescription
+ *
+ */
+
+import React, { PropTypes } from 'react'
+
+let EditableDescription = ({description, updateDescription}) => {
+  return (
+    <p>
+    {
+      description ||
+        <a
+          onClick={startEditing}
+          href='javascript:void(0)'>
+            Add description!
+        </a>
+    }
+    </p>
+  )
+}
+
+EditableDescription.propTypes = {
+  description: PropTypes.string,
+  updateDescription: PropTypes.func
+}
+
+export default EditableDescription
--- src/components/itemBase.js
+++ src/components/itemBase.js
@@ -4,12 +4,17 @@
  */
 
 import React, { PropTypes } from 'react'
+import EditableDescription from './editableDescription'
 
 let ItemBase = ({id, name, description, className, onMarketsItemUpdate}) => {
+  let updateDescription = onMarketsItemUpdate.bind(this, id)
   return (
     <article className={className}>
       <h3>{name}</h3>
-      <p>{description || 'Add description!'}</p>
+      <EditableDescription
+        description={description}
+        updateDescription={updateDescription}
+      />
     </article>
   )
 }
